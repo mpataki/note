1667760a1ac206e0151f8fdfa998c8f99fa83f32
Mat Pataki <matpataki@gmail.com>
2018-05-08 23:18:16 -0400

    bash completions on linux

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ function print_with_color() {
   printf "$color$string$NONE\n"
 }
 
+uname=`uname`
+
 print_with_color $GREEN 'Installing github/markup gem'
 gem install github-markup
 
@@ -23,9 +25,13 @@ print_with_color $GREEN 'Installing `note` to /usr/local/bin'
 cp ./note /usr/local/bin
 
 print_with_color $GREEN 'Installing bash completion script'
-cp `pwd`/bash_completion.d/note /usr/local/etc/bash_completion.d
+if [[ "$uname" == 'Linux' ]]; then
+  sudo mkdir /etc/bash_completion.d
+  sudo cp `pwd`/bash_completion.d/note /etc/bash_completion.d
+else
+  cp `pwd`/bash_completion.d/note /usr/local/etc/bash_completion.d
+fi
 
 print_with_color $GREEN 'Installing zsh completion script'
 mkdir -p ~/.zsh/completions
 cp `pwd`/zsh_completion/_note ~/.zsh/completions
-
